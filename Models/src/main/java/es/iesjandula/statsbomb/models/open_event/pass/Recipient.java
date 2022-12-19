931eafc43e6b94f88724a15f8674763925d859e6
jguegon966 <jguegon966@g.educaand.es>
2022-12-19 12:20:09 +0100

    commented classes Carry, FoulCommited, Heigth, Pass and recipient.

--- Models/src/main/java/es/iesjandula/statsbomb/models/open_event/pass/Recipient.java
+++ Models/src/main/java/es/iesjandula/statsbomb/models/open_event/pass/Recipient.java
@@ -2,39 +2,67 @@ package es.iesjandula.statsbomb.models.open_event.pass;
 
 /**
  * @author Neil Hdez
- * @version 1.0.0
- * @since 30/11/2022
+ * @author Jesus Guerrero
+ *
+ * specifying the player id of the recipient of
+ * the pass, or for whom an incomplete pass was intended.
  */
 public class Recipient
 {
-    private int id; // id
+    /**
+     * Attribute -
+     * id of the recipient
+     */
+    private int id;
+    /**
+     * Attribute -
+     * name of the recipient of the pass
+     */
     private String name; // name of the Position
 
+    /**
+     * Default constructor
+     */
     public Recipient ()
     {
 
     }
 
+    /**
+     * @return th id of the recipient
+     */
     public int getId()
     {
         return this.id;
     }
 
+    /**
+     * @param id of the recipient
+     */
     public void setId(int id)
     {
         this.id = id;
     }
 
+    /**
+     * @return the name of the recipient
+     */
     public String getName()
     {
         return this.name;
     }
 
+    /**
+     * @param name of the recipient
+     */
     public void setName(String name)
     {
         this.name = name;
     }
 
+    /**
+     * @return instance of recipient
+     */
     @Override
     public String toString()
     {
