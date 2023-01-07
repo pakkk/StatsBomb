8a652537991697111f1402ef7d1dcf476f9d4e03
darram1 <darmram@g.educaand.es>
2023-01-07 19:39:56 +0100

    Create ScorerFilter and ScorerResult

--- EventsStats/src/main/java/es/iesjandula/statsbomb/events_stats/scorer/ScorerFilter.java
+++ EventsStats/src/main/java/es/iesjandula/statsbomb/events_stats/scorer/ScorerFilter.java
@@ -1,4 +1,40 @@
 package es.iesjandula.statsbomb.events_stats.scorer;
 
-public class ScorerFilter {
+import es.iesjandula.statsbomb.common.exception.StatsBombException;
+import es.iesjandula.statsbomb.common.utils.JsonUtils;
+import es.iesjandula.statsbomb.models.event.Event;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author David Armijo
+ */
+public class ScorerFilter
+{
+    public String getScorers(List<Event> eventList) throws StatsBombException {
+
+        // result of the scorers
+        String jsonScorers = "";
+
+        List<ScorerResult> scorerResultList = new ArrayList<>();
+
+        for(Event event : eventList){
+            if(event.getShot() != null){
+                if(event.getShot().getOutcome().getName().equalsIgnoreCase("goal")){
+                    scorerResultList.add(new ScorerResult( event.getTeam().getName(),
+                            event.getPlayer().getName(), event.getMinute(), event.getSecond()));
+
+                }
+            }
+        }
+
+        // Convert the duel list to a json
+        JsonUtils jsonUtils = new JsonUtils();
+
+        jsonScorers = jsonUtils.writeObjectToJsonAsStringPretty(jsonScorers);
+
+        return jsonScorers;
+
+    }
 }
