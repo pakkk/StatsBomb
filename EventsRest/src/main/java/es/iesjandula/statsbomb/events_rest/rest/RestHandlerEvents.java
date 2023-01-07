3ea3f8a4567613a9049e575f17ec6c3baf42e25d
jmorsan426 <jmorsan426@g.educaand.es>
2023-01-07 20:35:15 +0100

    Create EventsStats , Launcher, RestHandlerEvents, edit Constants and the resource application.yaml

---
+++ EventsRest/src/main/java/es/iesjandula/statsbomb/events_rest/rest/RestHandlerEvents.java
@@ -0,0 +1,53 @@
+package es.iesjandula.statsbomb.events_rest.rest;
+
+import es.iesjandula.statsbomb.common.exception.StatsBombException;
+import es.iesjandula.statsbomb.events_stats.EventsStats;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * ------------------------------------------------
+ *
+ * @author API Rest Generator
+ * ------------------------------------------------
+ */
+@CrossOrigin(maxAge = 3600)
+@RequestMapping(value = "/events", produces = {"application/json"})
+@RestController //
+public class RestHandlerEvents
+{
+
+    private final EventsStats eventsStats = this.getEventsStats();
+
+    /**
+     * Method return Instance of CompetitionsStats
+     *
+     * @return Instance of CompetitionsStats
+     */
+    @Bean
+    private EventsStats getEventsStats()
+    {
+        return new EventsStats();
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/goalkeeper/")
+    public ResponseEntity<?> getGoalkeeperPlayer(@RequestParam(value="matchId", required=true) final Integer matchId)
+    {
+        try
+        {
+            String resultJson = this.eventsStats.getPlayerGoalkeeper(matchId);
+            return ResponseEntity.ok().body(resultJson) ;
+        }
+        catch (StatsBombException statsBombException)
+        {
+            return ResponseEntity.status(500).body(statsBombException.getBodyExceptionMessage());
+        }
+        catch (Exception exception)
+        {
+            StatsBombException statsBombException = new StatsBombException(exception);
+            return ResponseEntity.status(590).body(statsBombException.getBodyExceptionMessage());
+        }
+
+    }
+}
---
+++ EventsStats/src/main/java/es/iesjandula/statsbomb/events_stats/EventsStats.java
@@ -0,0 +1,66 @@
+package es.iesjandula.statsbomb.events_stats;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import es.iesjandula.statsbomb.common.exception.StatsBombException;
+import es.iesjandula.statsbomb.common.load_json.Json;
+import es.iesjandula.statsbomb.common.load_json.JsonLoaderImpl;
+import es.iesjandula.statsbomb.common.utils.Constants;
+import es.iesjandula.statsbomb.events_stats.goalkeeper.GoalkeeperFilter;
+import es.iesjandula.statsbomb.models.event.Event;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Joaquin Moreno
+ *
+ * In this class we create the filter and statistics for Json Events.
+ */
+public class EventsStats
+{
+    private final Logger LOGGER = LogManager.getLogger();
+
+    /**
+     * Return list with all events in json StatsBomb
+     *
+     * @return List of Events
+     * @throws StatsBombException
+     */
+    private List<Event> getListEvents(int matchId) throws StatsBombException
+    {
+
+        JsonLoaderImpl jsonLoader = new JsonLoaderImpl();
+        String jsonEvents = jsonLoader.loadEvents(matchId);
+        ObjectMapper mapper = Json.mapper();
+        List<Event> eventsList = new ArrayList<>();
+
+        try
+        {
+            eventsList = mapper.readValue(jsonEvents, new TypeReference<List<Event>>(){});
+        }
+        catch (JsonProcessingException jsonProcessingException)
+        {
+            LOGGER.error(Constants.E_PARSING_JSON_TO_OBJECT, jsonProcessingException);
+            throw new StatsBombException(Constants.E_PARSING_JSON_TO_OBJECT, "Error a la hora de parsear el Json", jsonProcessingException);
+        }
+
+        return eventsList;
+    }
+
+    /**
+     * This method show the goalkeeper who uses his feet the most .
+     *
+     * @return String in format Json Pretty with the goalkeeper who uses his feet the most.
+     * @throws StatsBombException
+     */
+    public String getPlayerGoalkeeper(int matchId) throws StatsBombException
+    {
+        GoalkeeperFilter goalkeeperFilter = new GoalkeeperFilter();
+        return goalkeeperFilter.getPlayerGoalkeeper(getListEvents(matchId));
+    }
+
+}
