cbfd6031781769c14b88cd5b57e617703a57630b
neilhdezs <hernandezsalvadorneil@gmail.com>
2023-02-13 21:12:42 +0100

    implement GateWay

---
+++ GateWay-Server/src/main/java/es/iesjandula/statsbomb/gateway_server/GateWayServerApplication.java
@@ -0,0 +1,13 @@
+package es.iesjandula.statsbomb.gateway_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GateWayServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GateWayServerApplication.class, args);
+	}
+
+}
---
+++ GateWay-Server/src/test/java/es/iesjandula/statsbomb/gateway_server/GateWayServerApplicationTests.java
@@ -0,0 +1,13 @@
+package es.iesjandula.statsbomb.gateway_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GateWayServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
