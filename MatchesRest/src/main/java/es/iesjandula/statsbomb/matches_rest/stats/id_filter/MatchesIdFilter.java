4709e5de681897950fc735e713a13d5ceb757a0c
jmorsan <jomorenosanchez@gmail.com>
2023-03-01 09:40:43 +0100

    change endpoints matches

--- MatchesRest/src/main/java/es/iesjandula/statsbomb/matches_rest/stats/id_filter/MatchesIdFilter.java
+++ MatchesRest/src/main/java/es/iesjandula/statsbomb/matches_rest/stats/id_filter/MatchesIdFilter.java
@@ -3,13 +3,20 @@ package es.iesjandula.statsbomb.matches_rest.stats.id_filter;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import es.iesjandula.statsbomb.common.exception.StatsBombException;
+import es.iesjandula.statsbomb.common.load_json.IJsonLoader;
+import es.iesjandula.statsbomb.common.load_json.Json;
+import es.iesjandula.statsbomb.common.load_json.JsonLoaderImpl;
 import es.iesjandula.statsbomb.common.utils.JsonUtils;
 import es.iesjandula.statsbomb.matches_rest.stats.matches_result_filters.MatchesResults;
 import es.iesjandula.statsbomb.models.matches.Match;
 
 public class MatchesIdFilter
 {
+    /**
     public String getMatchesId(List<Match> matchs) throws StatsBombException
     {
         List<MatchesResults> matchesResultsList = new ArrayList<MatchesResults>();
@@ -24,4 +31,15 @@ public class MatchesIdFilter
 
         return jsonUtils.writeObjectToJsonAsStringPretty(matchesResultsList);
     }
+     **/
+
+    public List<Match> getListMatch(int competitionId, int seasonId) throws StatsBombException, JsonProcessingException
+    {
+        IJsonLoader jsonLoader = new JsonLoaderImpl();
+
+        ObjectMapper mapper = Json.mapper();
+        return mapper.readValue(jsonLoader.loadMatches(competitionId, seasonId), new TypeReference<List<Match>>(){});
+    }
+
+
 }
